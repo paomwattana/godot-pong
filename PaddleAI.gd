1b0c21e565ca0505467b4a5bc1c97c2edcf780da
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-08-01 23:43:53 +0700

    tried paddle AI using rigidbody2d

--- PaddleAI.gd
+++ PaddleAI.gd
@@ -1,5 +1,24 @@
 extends RigidBody2D
 
+export (int) var speed = 600
+
+# consider custom signal
 
 func _ready():
 	pass
+
+func _physics_process(delta):
+	pass
+
+func _on_Area2D_body_entered(body:Node):
+	# if ball detected, move towards ball's position
+	if (body.name.match("**Ball***")):
+		if (body.position.y < self.position.y):
+			linear_velocity.y = -speed
+		if (body.position.y > self.position.y):
+			linear_velocity.y = speed
+
+
+func _on_Area2D_body_exited(body:Node):
+	pass
+	# mode = MODE_STATIC
