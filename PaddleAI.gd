7e5e3a406a8f64c503a9b3040c76997326d20320
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-08-02 22:49:40 +0700

    working opponent paddle AI

--- PaddleAI.gd
+++ PaddleAI.gd
@@ -1,6 +1,8 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 export (int) var speed = 600
+var ball = null
+var velocity = Vector2.ZERO
 
 # consider custom signal
 
@@ -8,17 +10,23 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	velocity = Vector2.ZERO
+	if (ball):
+		velocity = position.direction_to(ball.position) * speed
+		velocity.x = 0
+	velocity = move_and_slide(velocity)
 	pass
 
+
 func _on_Area2D_body_entered(body:Node):
-	# if ball detected, move towards ball's position
-	if (body.name.match("**Ball***")):
-		if (body.position.y < self.position.y):
-			linear_velocity.y = -speed
-		if (body.position.y > self.position.y):
-			linear_velocity.y = speed
+	ball = body
+	# # if ball detected, move towards ball's position
+	# if (body.name.match("**Ball***")):
+	# 	if (body.position.y < self.position.y):
+	# 		linear_velocity.y = -speed
+	# 	if (body.position.y > self.position.y):
+	# 		linear_velocity.y = speed
 
 
 func _on_Area2D_body_exited(body:Node):
-	pass
-	# mode = MODE_STATIC
+	ball = null
--- PaddlePlayer.gd
+++ PaddlePlayer.gd
@@ -8,9 +8,9 @@ var velocity = Vector2()
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
+		velocity.y = speed
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
+		velocity.y = -speed
 
 func _physics_process(delta):
 	get_input()
