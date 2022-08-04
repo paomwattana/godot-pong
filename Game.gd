3c1e58a7637e88dc07434ab728bc8ef636212386
paomwattana <prachnachai.mek@student.mahidol.edu>
2022-08-04 15:54:56 +0700

    functioning game over menu

--- Game.gd
+++ Game.gd
@@ -1,36 +1,39 @@
 extends Node2D
 
 const ballResource = preload("res://Ball.tscn")
-const gameOverMenuResource = preload("res://PaddleAI.tscn")
+const gameOverMenuResource = preload("res://GameOverMenu.tscn")
 
 onready var scorePlayer = 0
 onready var scoreAI = 0
 onready var ballSpawnTimer = $BallSpawnTimer
 export var WINNING_SCORE = 2
 
+
+
 func _ready():
 	ballSpawnTimer.start()
 
 func checkGameOver():
 	if (WINNING_SCORE <= scorePlayer || WINNING_SCORE <= scoreAI):
+		
 		var gameOverMenu = gameOverMenuResource.instance()
 		if (WINNING_SCORE <= scorePlayer): # player wins
 			pass
 		else: # AI wins
 			pass
 		self.call_deferred("add_child", gameOverMenu)
+	else:
+		ballSpawnTimer.start()
 
 func _on_GoalLeft_area_entered(_body):
 	scoreAI += 1
 	$Scores/AIScore.text = str(scoreAI)
 	checkGameOver()
-	ballSpawnTimer.start()
 
 func _on_GoalRight_area_entered(_body):
 	scorePlayer += 1
 	$Scores/PlayerScore.text = str(scorePlayer)
 	checkGameOver()
-	ballSpawnTimer.start()
 
 
 func _on_BallSpawnTimer_timeout():
